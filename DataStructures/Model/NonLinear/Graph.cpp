c38eb5566e44685e424c6ad51d915eb1f7188f48
Shahriar <ssha7332@gmail.com>
2019-03-07 12:38:30 -0700

    I added some of the semi colons and the parens for two of the applications. I also added some small codes and projects.

---
+++ DataStructures/Model/NonLinear/Graph.cpp
@@ -0,0 +1,9 @@
+//
+//  Graph.cpp
+//  DataStructures
+//
+//  Created by Shams, Shahriar on 3/6/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Graph.hpp"
---
+++ DataStructures/Model/NonLinear/Graph.hpp
@@ -0,0 +1,165 @@
+//
+//  Graph.hpp
+//  DataStructures
+//
+//  Created by Shams, Shahriar on 3/6/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <assert.h>
+#include <set>
+#include <queue>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+    
+
+
+private:
+static const int MAXIMUM = 50;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    
+    void addVertex(const Type& value);
+    
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target, int cost);
+    void removeEdge(int source, int target);
+    void removeEdgeUndirected(int source, int target);
+    void removeEdgeCost(int source, int target);
+    
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target)const;
+    std::set<int> neighbors(int vertex) const;
+    
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+};
+    template <class Type>
+    const int Graph<Type> :: MAXIMUM;
+    
+    template <class Type>
+    Graph<Type> :: Graph()
+    {
+        this->vertexCount = 0;
+    }
+    
+template <class Type>
+int Graph<Type> :: size() const
+{
+    return vertexCount;
+}
+
+template <class Type>
+Type& Graph<Type> :: operator[](int vertex)
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+
+template <class Type>
+Type Graph<Type> :: operator[](int vertex) const
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+
+template <class Type>
+void Graph<Type> :: addVertex(const Type& value)
+{
+    assert(vertexCount < MAXIMUM);
+    int newVertexNumber = vertexCount;
+    vertexCount++;
+    
+    for(int otherVertexNumber = 0; otherVertexNumber < vertexCount; otherVertexNumber++)
+    {
+        adjacencyMatrix[otherVertexNumber][newVertexNumber] = false;
+        adjacencyMatrix[newVertexNumber][otherVertexNumber] = false;
+    
+    }
+    graphData[newVertexNumber] = value;
+}
+
+template <class Type>
+void Graph<Type> :: addEdge(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = true;
+}
+
+template <class Type>
+void Graph<Type> :: addEdgeCost(int source, int target, int cost)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    weightCostMatrix[source][target] = cost;
+    weightCostMatrix[target][source] = cost;
+}
+
+template <class Type>
+void Graph<Type> :: addEdgeUndirected(int source, int target)
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    adjacencyMatrix[source][target] = true;
+    adjacencyMatrix[target][source] = true;
+}
+
+template <class Type>
+bool Graph<Type> :: hasUndirectedConnection(int source, int target) const
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target] || adjacencyMatrix[target][source] ;
+    
+    return isAnEdge;
+}
+
+template <class Type>
+bool Graph<Type> :: areConnected(int source, int target) const
+{
+    assert(source >= 0 && source < vertexCount && target >= 0 && target < vertexCount);
+    
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source][target];
+    
+    return isAnEdge;
+}
+
+template <class Type>
+std::set<int> Graph<Type> :: neighbors(int vertex) const
+{
+    assert(vertex < vertexCount);
+    std::set<int> vertexNeighbors;
+    
+    for(int index = 0; index < vertexCount; index++)
+    {
+        if(adjacencyMatrix[vertex][index])
+        {
+            vertexNeighbors.insert(index);
+        }
+}
+    return vertexNeighbors;
+}
+
+template <class Type>
+void Graph<Type> :: depthFirstTraversal(Graph<Type> & currentGraph, int vertex)
+
+
+
+#endif /* Graph_hpp */
